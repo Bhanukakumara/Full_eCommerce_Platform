d630ced15e47589d37e838682e8581b693a653d3
Bhanukakumara <bhanukarasika@gmail.com>
2025-05-13 21:36:05 +0530

    initialization

---
+++ src/main/java/edu/bks/full_ecommerce_platform/FullECommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package edu.bks.full_ecommerce_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullECommercePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullECommercePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bks/full_ecommerce_platform/FullECommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bks.full_ecommerce_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullECommercePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
