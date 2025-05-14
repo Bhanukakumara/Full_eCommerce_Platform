3eb654efaba0114d9eb2066537493f00772a9ea0
Bhanukakumara <bhanukarasika@gmail.com>
2025-05-14 16:49:56 +0530

    update address entity

--- src/main/java/edu/bks/full_ecommerce_platform/entities/Address.java
+++ src/main/java/edu/bks/full_ecommerce_platform/entities/Address.java
@@ -1,16 +1,19 @@
 package edu.bks.full_ecommerce_platform.entities;
 
+import edu.bks.full_ecommerce_platform.enums.AddressType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Table(name = "addresses'")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -20,34 +23,64 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "unit_number")
+    @Column(name = "unit_number", length = 20)
+    @Size(max = 20, message = "Unit number cannot exceed 20 characters")
     private String unitNumber;
 
-    @Column(name = "street_number")
+    @Column(name = "street_number", length = 20)
+    @Size(max = 20, message = "Street number cannot exceed 20 characters")
     private String streetNumber;
 
-    @Column(name = "address_line1", nullable = false)
+    @Column(name = "address_line_1", nullable = false, length = 100)
     @NotBlank(message = "Address line 1 is required")
+    @Size(min = 3, max = 100, message = "Address line 1 must be between 3 and 100 characters")
     private String addressLine1;
 
-    @Column(name = "address_line2")
+    @Column(name = "address_line_2", length = 100)
+    @Size(max = 100, message = "Address line 2 cannot exceed 100 characters")
     private String addressLine2;
 
-    @Column(nullable = false)
+    @Column(name = "city", nullable = false, length = 50)
     @NotBlank(message = "City is required")
+    @Size(min = 2, max = 50, message = "City must be between 2 and 50 characters")
     private String city;
 
-    private String region;
+    @Column(name = "state_province", length = 50)
+    @Size(max = 50, message = "State/Province cannot exceed 50 characters")
+    private String stateProvince;
 
-    @Column(name = "postal_code", nullable = false)
+    @Column(name = "postal_code", nullable = false, length = 20)
     @NotBlank(message = "Postal code is required")
-    private String postalCode;
+    @Size(max = 5, message = "Postal code must be between 3 and 20 characters")
+    private int postalCode;
+
+    @Column(name = "address_type", length = 20)
+    @Enumerated(EnumType.STRING)
+    private AddressType addressType;
+
+    @Column(name = "delivery_instructions", length = 500)
+    @Size(max = 500, message = "Delivery instructions cannot exceed 500 characters")
+    private String deliveryInstructions;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_by",updatable = false)
+    private Long createdBy;
+
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @OneToMany(mappedBy = "address", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserAddress> userAddresses;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "country_id", nullable = false)
     @NotNull(message = "Country is required")
     private Country country;
-
-    @OneToMany(mappedBy = "address", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<UserAddress> userAddresses;
 }
---
+++ src/main/java/edu/bks/full_ecommerce_platform/enums/AddressType.java
@@ -0,0 +1,11 @@
+package edu.bks.full_ecommerce_platform.enums;
+
+public enum AddressType {
+    SHIPPING,
+    BILLING,
+    BOTH,
+    WAREHOUSE,
+    STORE,
+    HEADQUARTERS,
+    OTHER
+}
