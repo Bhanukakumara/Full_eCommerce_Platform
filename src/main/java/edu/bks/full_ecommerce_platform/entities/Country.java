2beb966072f6d2685f2f42a44eaac2704274fcf2
Bhanukakumara <bhanukarasika@gmail.com>
2025-05-14 17:05:52 +0530

    update country entity

--- src/main/java/edu/bks/full_ecommerce_platform/entities/Country.java
+++ src/main/java/edu/bks/full_ecommerce_platform/entities/Country.java
@@ -2,15 +2,21 @@ package edu.bks.full_ecommerce_platform.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(name = "country")
+@Table(name = "countries")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -20,10 +26,31 @@ public class Country {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "name", nullable = false, unique = true, length = 100)
     @NotBlank(message = "Country name is required")
+    @Size(min = 2, max = 100, message = "Country name must be between 2 and 100 characters")
     private String name;
 
+    @Column(name = "country_code", nullable = false, unique = true, length = 2)
+    @NotBlank(message = "Country code is required")
+    @Size(min = 2, max = 2, message = "Country code must be exactly 2 characters.")
+    @Pattern(regexp = "^[A-Z]{2}$", message = "Country code must be 2 uppercase letters.")
+    private String countryCode;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
     @OneToMany(mappedBy = "country", cascade = CascadeType.ALL)
-    private List<Address> addresses;
+    private Set<Address> addresses = new HashSet<>();
 }
